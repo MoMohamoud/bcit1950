ac1ce6c62cbac378755407570c264baff29effca
MoMohamoud <moeddie@hotmail.com>
2015-06-17 22:29:23 -0700

    continued working on the right dashboard

--- index.php
+++ index.php
@@ -4,18 +4,18 @@
 <div class="container no-mrg no-padding width">
 	<div class="row ">
 	
-		<div class="col-sm-2 col-xs-4 relative">
+		<div class="col-sm-3 col-xs-3 relative">
 			<section class="sidebar bg-color">
 				<?php include 'templates/dashboard.html';?>
 			</section>
 		</div>
 
 
-		<div class="col-sm-8 col-xs-5">
+		<div class="col-sm-6 col-xs-6">
 			<?php include 'templates/content.html';?>
 		</div>
 
-		<div class="col-sm-2 col-xs-3 relative">
+		<div class="col-sm-3 col-xs-3 relative">
 
 		<section class="right-box">
 				<aside class="pd-t">
