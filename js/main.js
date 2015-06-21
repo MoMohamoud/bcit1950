41164af50c17c32f6be0f587b0afdb3c490d2efb
MoMohamoud <moeddie@hotmail.com>
2015-06-21 16:46:53 -0700

    created the front created footer and added the links created the mock chat area

--- js/main.js
+++ js/main.js
@@ -15,4 +15,39 @@ $('.open-popup-link').magnificPopup({
  $( "#login-info" ).toggle();
  
  });
+
+	 $('.count').each(function () {
+	    $(this).prop('Counter',0).animate({
+	        Counter: $(this).text()
+	    }, {
+	        duration: 2000,
+	        easing: 'swing',
+	        step: function (now) {
+	            $(this).text(Math.ceil(now));
+	        }
+	    });
+	});
+
+	$(".dropdown").hover(function (event) {
+	    $('.glyphicon', this).toggleClass('rotate');
+	    $('.glyphicon', this).toggleClass('rotate2');
+	});
+
+	 // // ADD SLIDEDOWN ANIMATION TO DROPDOWN //
+  // $('.dropdown').click(function(e){
+  //   $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+  // });
+
+  // //ADD SLIDEUP ANIMATION TO DROPDOWN //
+  // $('.dropdown').click(function(e){
+  //   $(this).find('.open').first().stop(true, true).slideUp();
+  // });
+
+	$('.dropdown').hover(function() {
+		$(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+	}, function() {
+		$(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
+	});
+
 });
+
