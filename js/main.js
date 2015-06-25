d493472f9a234b12039c459c5faf060d91c916e8
MoMohamoud <moeddie@hotmail.com>
2015-06-24 18:36:35 -0700

    quiz timer is now working, cleaned up all the accordion code

--- js/main.js
+++ js/main.js
@@ -1,68 +1,52 @@
 $(document).ready(function() {
 
-function accordionInit() {
-    $("#accordion").accordion();
-};
+	function accordionInit() {
+	    $("#accordion").accordion();
+	};
 
-function accordionDestroy() {
-    $("#accordion").accordion("destroy");
-};
-$(function() {
-    $( "#accordion" ).accordion({
-	collapsible: true,
-	  active: 0,
-	  animate: 400,
-	heightStyle: "content"
-		// event: "mouseover"
-  });
+	function accordionDestroy() {
+	    $("#accordion").accordion("destroy");
+	};
+	$(function() {
+	    $( "#accordion" ).accordion({
+		collapsible: true,
+		  active: 0,
+		  animate: 400,
+		heightStyle: "content"
+			// event: "mouseover"
+	  });
 
-});
-function accordionInit() {
-    $("#accordion").accordion();
-};
-
-function accordionDestroy() {
-    $("#accordion").accordion("destroy");
-};
-$(function() {
-    $( "#accordion" ).accordion({
-	collapsible: true,
-	  active: 0,
-	  animate: 400,
-	heightStyle: "content"
-		// event: "mouseover"
-  });
-
-});
+	});
 
    
    
-$('.open-popup-link').magnificPopup({
-  focus:'#email',
-  type:'inline',
-  midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-  callbacks: {
-			beforeOpen: function() {
-				if($(window).width() < 700) {
-					this.st.focus = false;
-				} else {
-					this.st.focus = '#email';
-				}
-			}
-  }
-  });
- $('#submit-login').click(function(){
-  var email = $('#email').val();
-  
+	$('.open-popup-link').magnificPopup({
+		focus:'#email',
+		type:'inline',
+		midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+		callbacks: {
+		  	beforeOpen: function() {
+		  		if($(window).width() < 700) {
+		  			this.st.focus = false;
+		  		} else {
+		  			this.st.focus = '#email';
+		  		}
+		  	}
+		}
+	});
 
-  
- });
- $('#user-option').click(function(){
- $( ".open-popup-link" ).toggle();
- $( ".menu-l" ).toggle();
- $( "#login-info" ).toggle();
- 
- });
+
+
+	$('#submit-login').click(function(){
+	  var email = $('#email').val(); 
+		 });
+	$('#user-option').click(function(){
+		$( ".open-popup-link" ).toggle();
+		$( ".menu-l" ).toggle();
+		$( "#login-info" ).toggle();
+	});
+
+
 
 	 $('.count').each(function () {
 	    $(this).prop('Counter',0).animate({
@@ -76,26 +60,23 @@ $('.open-popup-link').magnificPopup({
 	    });
 	});
 
+
+
 	$(".dropdown").hover(function (event) {
 	    $('.glyphicon', this).toggleClass('rotate');
 	    $('.glyphicon', this).toggleClass('rotate2');
 	});
 
-	 // // ADD SLIDEDOWN ANIMATION TO DROPDOWN //
-  // $('.dropdown').click(function(e){
-  //   $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
-  // });
 
-  // //ADD SLIDEUP ANIMATION TO DROPDOWN //
-  // $('.dropdown').click(function(e){
-  //   $(this).find('.open').first().stop(true, true).slideUp();
-  // });
+
 
 	$('.dropdown').hover(function() {
 		$(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
 	}, function() {
 		$(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
 	});
+
+	
 
 });
 
--- js/quiz.js
+++ js/quiz.js
@@ -1,27 +1,41 @@
 
-var seconds_left = 70;
-var interval = setInterval(function() {
-    seconds_left;
-	if (seconds_left < 60){
-	document.getElementById('timeLeft').innerHTML = "Time left:" + seconds_left; 
-	}
-    if (seconds_left <= 30)
-    {
-		setTimeout(window.location="http://www.bcitcomp.ca",10);
-        document.getElementById('timeLeft').innerHTML = "You are ready";
-        clearInterval(interval);
-    }
+// var seconds_left = 70;
+// var interval = setInterval(function() {
+//     seconds_left;
+// 	if (seconds_left < 60){
+// 	document.getElementById('timeLeft').innerHTML = "Time left:" + seconds_left; 
+// 	}
+//     if (seconds_left <= 30)
+//     {
+// 		setTimeout(window.location="http://www.bcitcomp.ca",10);
+//         document.getElementById('timeLeft').innerHTML = "You are ready";
+//         clearInterval(interval);
+//     }
+// var seconds_left = 70;
+// var interval = setInterval(function() {
+//     --seconds_left;
+// 	if (seconds_left < 60){
+// 	document.getElementById('timeLeft').innerHTML = "Time left:" + seconds_left; 
+// 	}
+//     if (seconds_left <= 30)
+//     {
+// 		setTimeout(window.location="http://www.bcitcomp.ca",10);
+//         document.getElementById('timeLeft').innerHTML = "You are ready";
+//         clearInterval(interval);
+//     }
+
+// }, 1000);
+
 var seconds_left = 70;
 var interval = setInterval(function() {
     --seconds_left;
-	if (seconds_left < 60){
-	document.getElementById('timeLeft').innerHTML = "Time left:" + seconds_left; 
-	}
+    if (seconds_left < 60){
+    document.getElementById('timeLeft').innerHTML = "Time left:" + seconds_left; 
+    }
     if (seconds_left <= 30)
     {
-		setTimeout(window.location="http://www.bcitcomp.ca",10);
+        setTimeout(window.location="http://www.bcitcomp.ca",10);
         document.getElementById('timeLeft').innerHTML = "You are ready";
         clearInterval(interval);
     }
-
 }, 1000);
